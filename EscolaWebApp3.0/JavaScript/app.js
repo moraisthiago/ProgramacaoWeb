bbbe9d88f14c3c3b636c18e26048819fa69f3fca
moraisthiago <thiagueson@gmail.com>
2019-10-10 15:12:31 -0300

    Controller

---
+++ EscolaWebApp3.0/JavaScript/app.js
@@ -0,0 +1,68 @@
+//Inicializando Módulos.
+let nomeApp = "EscolaWebApp3.0";
+let modulos = [];
+var app = angular.module(nomeApp, modulos);
+
+//Estrutura básica de uma função para um controller.
+var alunoController = function($scope){
+  $scope.nome = "";
+  $scope.matricula = "";
+  $scope.cpf = "";
+  $scope.nascimento = "";
+
+  app.controller('AlunoController', alunoController);
+}
+
+var campusController = function($scope){
+  $scope.silga = "";
+  $scope.cidade = "";
+
+  app.controller('CampusController', campusController);
+}
+
+var cursoController = function($scope){
+  $scope.nome = "";
+  $scope.turno = "";
+
+  app.controller('CursoController', cursoController);
+}
+
+var disciplinaControler = function($scope){
+  $scope.nome = "";
+
+  app.controller('DisciplinaController', disciplinaController);
+}
+
+var enderecoController = function($scope){
+  $scope.logradouro = "";
+  $scope.complemento = "";
+  $scope.bairro = "";
+  $scope.cep = "";
+  $scope.numero = "";
+
+  app.controller('EnderecoController', enderecoController);
+}
+
+var escolaController = function($scope){
+  $scope.nome = "";
+
+  app.controller('EscolaController', escolaController);
+}
+
+var professorController = function($scope){
+  $scope.nome = "";
+
+  app.controller('ProfessorController', professorController);
+}
+
+var turmaController = function($scope){
+  $scope.nome = "";
+
+  app.controller('TurmaController', turmaController);
+}
+
+var turnoController = function($scope){
+  $scope.nome = "";
+
+  app;controller('TurnoController', turnoController);
+}
